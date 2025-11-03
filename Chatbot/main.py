da0d9b72951494cf6f35782ca450fe04a0412d49
Gideon <gideonadurota@outlook.com>
2025-11-03 10:23:06 -0600

    add json and logic to close to chatbot

--- Chatbot/main.py
+++ Chatbot/main.py
@@ -1,4 +1,5 @@
 from difflib import get_close_matches
+import json
 
 def get_best_match(user_question: str, questions: dict) -> str | None:
     questions: list[str] = [q for q in questions]
@@ -13,15 +14,20 @@ def chatbot(knowledge: dict):
         best_match: str | None = get_best_match(user_input, knowledge)
 
         if answer := knowledge.get(best_match):
+            if best_match == 'bye':
+                print(f'Bot: {answer}')
+                break
             print(f'Bot: {answer}')
         else:
             print('Bot: I do not understand that')
 
 if __name__ == '__main__':
-    brain: dict = {'hello': 'hey there',
-                   'how are you': 'thanks',
-                   'what time is it': 'Don\'t know, don\'t care',
-                   'bye': 'see you!'}
+    with open('questions.json', 'r', encoding='utf-8') as f:
+        brain: dict = json.load(f)
+        # brain: dict = {'hello': 'hey there',
+    #                'how are you': 'thanks',
+    #                'what time is it': 'Don\'t know, don\'t care',
+    #                'bye': 'see you!'}
     chatbot(knowledge=brain)
 
 
